a1229ee4c3304d17ce44085f3952e692bb2a7ecb
JeromeDelaire <delaire.jerome@hotmail.fr>
2024-04-26 17:52:01 +0200

    fix config

--- src/components/NavButton/index.tsx
+++ src/components/NavButton/index.tsx
@@ -10,7 +10,7 @@ export const NavButton: React.FC<NavButtonProps> = ({ title, current, onClick })
   return (
     <div
       onClick={() => onClick && onClick()}
-      className={`block py-2 pl-3 pr-4 ${
+      className={`flex py-2 pl-3 pr-4 ${
         current ? "text-primary dark:text-secondary font-bold" : "text-black dark:text-white"
       } cursor-pointer hover:text-primary hover:dark:text-secondary`}
     >
