caff29c430b7551685f086b81e08d6f3e3498ace
JeromeDelaire <delaire.jerome@hotmail.fr>
2024-04-30 14:53:13 +0200

    update build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -152,6 +152,7 @@ interface SelectLanguageProps {
     languages: string[];
     onLanguageClicked: (language: string) => void;
     currentValue: string;
+    size: "sm" | "md" | "lg" | "xl" | "2xl" | "3xl" | "4xl" | "5xl";
 }
 declare const SelectLanguage: React.FC<SelectLanguageProps>;
 
