3f544b66753b1456fbc7389583726e83ca72eb23
François-Xavier Thoorens <fx.thoorens@gmail.com>
2024-08-28 12:41:04 +0200

    fixed import

--- src/components/Table/Table.stories.tsx
+++ src/components/Table/Table.stories.tsx
@@ -1,5 +1,4 @@
 import { Meta, StoryObj } from "@storybook/react";
-import "../../../dist/index.css";
 import { ColumnType, Table } from ".";
 const meta: Meta<typeof Table> = {
   component: Table,
